7c5caac49ca7f7d8a1be21635dbcfb41ecab0dcb
William Wilson <williambwilson@msn.com>
2022-10-20 16:55:05 -0400

    addd analysis queries

---
+++ analysis queries.py
@@ -0,0 +1,38 @@
+import json
+from time import sleep
+from datetime import datetime
+import mysql.connector
+
+username = None
+password = None
+time_to_wait = 0.01
+username = ""
+password = ""
+
+with open('config.json') as f:
+    data = json.load(f)
+    username = data['username']
+    password = data['password']
+
+print(username)
+print(password)
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user=username,
+  password=password,
+  database="transactions"
+)
+
+
+#select queries for mysql
+#start timer
+
+#query
+
+#stop timer
+
+
+
+#select queries for clickhouse
