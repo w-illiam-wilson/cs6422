e06b6c21251a9793e549923402833221b07be1bc
vincent <huangvincent170@gmail.com>
2022-10-23 07:37:01 -0400

    modified add_rows script to use dataset, updated readme accordingly

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 2. Install Clickhouse: https://clickhouse.com/clickhouse#getting_started
 3. Add your config using template and renaming file "config.json"
 4. Make sure you have Python 3 (Code is specifically known to support 3.9), and you run `pip install -r requirements.txt` in the project directory
+5. Download the [dataset](https://www.kaggle.com/datasets/debashis74017/stock-market-data-nifty-100-stocks-5-min-data) and extract its contents to `data/`
 
 ## DB Setup For WSL
 Here's some general setup instructions for DB Setup on WSL:
@@ -14,6 +15,7 @@ Here's some general setup instructions for DB Setup on WSL:
 5. Run `clickhouse-client --password <your_password_here> --queries-file create_database_clickhouse.sql`
 
 ## Script Descriptions
-1. Use `add_rows` to add data to the MySQL database
-2. Use `migrate_to_clickhouse` to migrate data from MySQL to Clickhouse
-3. TODO describe others once complete
+1. run `mysql -u [username] -p < create_database.sql` to create the MySQL databse
+2. Use `add_rows` to simulate users adding data (OLTP) to the MySQL database
+3. Use `migrate_to_clickhouse` to migrate data from MySQL to Clickhouse
+4. TODO describe others once complete
--- add_rows.py
+++ add_rows.py
@@ -1,21 +1,46 @@
-from general import load_mysql_args, print_progress
+from general import load_mysql_args, print_progress, DATA_FILE_NAMES, COLUMN_NAMES
 from time import sleep
 from datetime import datetime
+from threading import Thread
 import mysql.connector
-
-mydb = mysql.connector.connect(**load_mysql_args())
-
-mycursor = mydb.cursor()
-
+        
 time_to_wait = 0.0001
-num_transactions = 100
 
-for i in range(num_transactions):
-    sleep(time_to_wait)
-    print_progress("writing to MySQL", i, num_transactions)
-    now = datetime.now()
-    formatted_date = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-    sql = "INSERT INTO purchases (time_purchase, customer_id, cost, store_num) VALUES (%s, %s, %s, %s)"
-    val = (formatted_date, 1, 2, 3)
-    mycursor.execute(sql, val)
-    mydb.commit()
+def write_data(conn, cursor, data_file):
+    with open (data_file, "r") as df:
+        i = 0
+        for line in df:
+            if i == 0:
+                i = 1
+                continue
+            if i > 10:
+                break
+            print(data_file)
+
+            # the csv has the datetime in the format
+            # YYYY-MM-DD HH:MM:SS+5:30
+            # but we need it in
+            # 'YYYY-MM-DD HH:MM'
+            datetime_end_idx = line.find(',')
+            assert(datetime_end_idx != -1)
+            plus_idx = line.find('+')
+            assert(plus_idx != -1)
+            assert(plus_idx < 100)
+            sql = f"INSERT INTO stock_info VALUES (\'{line[:plus_idx]}\'{line[datetime_end_idx:]})"
+            print(sql)
+            cursor.execute(sql)
+            conn.commit()
+            sleep(time_to_wait)
+            i += 1
+
+threads = []
+for data_file_name in DATA_FILE_NAMES:
+    conn = mysql.connector.connect(**load_mysql_args())
+    cursor = conn.cursor()
+    threads.append(Thread(target = write_data, args=(conn, cursor, data_file_name)))
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
