7b521e8d9b3351b2835844d99770e9ebafbeae31
Alexander Manning <amanning32@gatech.edu>
2022-12-05 18:10:31 -0500

    Add support for determining database from query in transaction processor

--- transaction_processor.py
+++ transaction_processor.py
@@ -2,6 +2,7 @@ from util import load_mysql_args, load_clickhouse_args
 import time
 import mysql.connector
 from clickhouse_driver import Client
+import sqlparse
 
 def whichTo(selection):
     if selection in ["olap", "ola", "ap", "a"]:
@@ -9,6 +10,49 @@ def whichTo(selection):
     elif selection in ["oltp", "olt", "tp", "t"]:
         return "oltp"
     else:
+        return None
+
+# Adapted from https://stackoverflow.com/questions/60822203/how-to-parse-any-sql-get-columns-names-and-table-name-using-sql-parser-in-python
+def get_query_columns(stmt):
+    columns = set()
+    column_identifiers = []
+
+    # get column_identifieres
+    in_select = False
+    for token in stmt.tokens:
+        if isinstance(token, sqlparse.sql.Comment):
+            continue
+        if str(token).lower() == 'select':
+            in_select = True
+        elif in_select and token.ttype == sqlparse.tokens.Wildcard:
+            columns.add("*")
+        elif in_select and token.ttype is None:
+            if isinstance(token, sqlparse.sql.IdentifierList):
+                for identifier in token.get_identifiers():
+                    column_identifiers.append(identifier)
+            elif isinstance(token, sqlparse.sql.Identifier):
+                column_identifiers.append(token)
+            break
+
+    # get column names
+    for column_identifier in column_identifiers:
+        columns.add(column_identifier.get_name())
+
+    return columns
+    
+def select_db_from_query(query):
+    parsed_query = sqlparse.parse(query)
+    operator = parsed_query[0].get_type()
+    print(operator.lower())
+    if operator.upper() == 'SELECT':
+        query_columns = get_query_columns(parsed_query[0])
+        if "*" in query_columns or len(query_columns) > 30:
+            return "oltp"
+        return "olap"
+    elif operator.upper() in ['INSERT', 'UPDATE', 'DELETE']:
+        return "oltp"
+    else:
+        print(f"Error selecting database for querying. Operator identified was {operator}")
         return None
 
 time_to_wait = 0.01
@@ -23,11 +67,12 @@ query = ""
 
 while(True):
     query = input("Type any query: ")
-    which = None
+    which = select_db_from_query(query)
     while which is None:
-        chosen = input("olap (a) or oltp (t): ")
-        which = whichTo(chosen)
+        query = input("Error parsing query, please retry: ")
+        which = select_db_from_query(query)
     try:
+        print(f"Targetting database for {which}")
         tic = time.perf_counter()
         if which == "oltp":
             mycursor.execute(query)
@@ -38,7 +83,7 @@ while(True):
         print(stock_info)
         print(f"Executed in {toc - tic:0.4f} seconds")
     except:
-        print("failed to process query")
+        print("Failed to process query")
 
 
 #select queries for mysql
